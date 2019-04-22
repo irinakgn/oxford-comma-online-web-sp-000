3728c1ab778b11c6bd63c515fe155b52b4621250
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 02:04:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length ==1
 elsif array.length == 2
   return array.join(' and ')
 else array.length === 3
-  return array[0, array.length - 1].join(', ') + ' and ' + array.last
+  return array[0, array.length - 1].join(', ') + ', and ' + array.last
 end
 
 end
